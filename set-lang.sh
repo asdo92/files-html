36c9b13daa1322af876bdb1df4dc92845e447396
q3aql <q3aql@duck.com>
2024-12-16 20:47:47 +0100

    Set language index script

---
+++ set-lang.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+dir=$(dirname $0)
+lang_dir="$(dirname $0)/lang"
+
+# List available languages
+lang_available=$(ls -1 ${lang_dir}/ 2> /dev/null | cut -d "." -f 1)
+
+# Set language
+if [ -z "${1}" ] ; then
+  echo ""
+  echo "* Set language for html indexer"
+  echo ""
+  echo "Sintax: $0 <lang>"
+  echo ""
+  echo "Available lang:" ${lang_available}
+  echo ""
+else
+  if [ -f "${lang_dir}/${1}.html" ] ; then
+    cp -rf "${lang_dir}/${1}.html" "${dir}/res/head.html"
+    echo "Language '${1}' set correctly"
+    echo "Run 'make-html-browser.sh' for to generate the html files"
+    exit 0
+  else
+    echo "Language '${1}' not available"
+    exit 1
+  fi
+fi
+
